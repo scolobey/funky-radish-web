cde76e02839055834398193a725befc671dfdc0d
Ryan Goodwin <minedied@gmail.com>
2022-02-12 21:30:53 -0700

    webpack plugin to shrink some files.

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,15 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin({ // <-- key to reducing React's size
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(), //dedupe similar code
+      new webpack.optimize.UglifyJsPlugin(), //minify everything
+      new webpack.optimize.AggressiveMergingPlugin()//Merge chunks
+    ]
 };
